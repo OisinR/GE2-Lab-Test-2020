8f448c2872b6eb5be99ed4e1b4aa7ae237245c1e
Oisin <C17343216@mydit.ie>
2020-04-21 10:16:10 +0100

    Lights Spawn

---
+++ Assets/Scripts/SpawnLights.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLights : MonoBehaviour
+{
+    public float radius = 20;
+    public float noOfLights;
+    List<GameObject> elements = new List<GameObject>();
+
+
+    void Start()
+    {
+        float theta = Mathf.PI * 2.0f / noOfLights;
+        for (int i = 0; i < noOfLights; i++)
+        {
+            Vector3 p = new Vector3(Mathf.Sin(theta * i) * radius, 0, Mathf.Cos(theta * i) * radius);
+            p = transform.TransformPoint(p);
+            Quaternion q = Quaternion.AngleAxis(theta * i * Mathf.Rad2Deg, Vector3.up);
+            q = transform.rotation * q;
+
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cube.transform.SetPositionAndRotation(p, q);
+            cube.transform.parent = this.transform;
+            cube.GetComponent<Renderer>().material.color = Color.yellow;
+            elements.Add(cube);
+        }
+    }
+
+
+
+
+	private void Update()
+    {
+        
+    }
+
+
+
+}
